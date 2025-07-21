609e169f79beca4ea8def6948a6152beaeb8ca1d
Paulo Cesar <paulocgsantos99@gmail.com>
2025-07-21 17:33:01 -0300

    feat: add handlers list and delete

--- handlers/deleteOpening.go
+++ handlers/deleteOpening.go
@@ -1,12 +1,30 @@
-package handlers 
+package handlers
 
 import (
+	"crud-oportunides/schemas"
 	"net/http"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"mgs": "DeleteOpeningHandler called",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, "Missing 'id' URL parameter")
+		return
+	}
+	
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("Opening with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("Error deleting opening with id: %s", id))
+		return 
+	}
+	sendSuccess(ctx, "delete-opening", opening)
 }
--- handlers/listOpening.go
+++ handlers/listOpening.go
@@ -1,12 +1,19 @@
-package handlers 
+package handlers
 
 import (
+	"crud-oportunides/schemas"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"mgs": "ListOpeningsHandler called",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "Error retrieving openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
