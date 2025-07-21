7452dda31e03d00faf579ba4e089c1ea892de58f
Paulo Cesar <paulocgsantos99@gmail.com>
2025-07-21 11:45:10 -0300

    feat: adjust code requests in create handler

--- handlers/request.go
+++ handlers/request.go
@@ -1,10 +1,38 @@
-package handlers 
+package handlers
+
+import "fmt"
+
+func errParmIsRequired(name, typ string) error {
+	return fmt.Errorf("parameter %s (type: %s) is required", name, typ)
+}
 
 type CreateOpeningRequest struct {
-	Role string `json:"role"`
-	Company string `json:"company"`
+	Role     string `json:"role"`
+	Company  string `json:"company"`
 	Location string `json:"location"`
-	Remote bool `json:"remote"`
-	Link string `json:"link"`
-	Salary int64 `json:"salary"`
+	Remote   bool   `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" {
+		return errParmIsRequired("Role", "string")
+	}
+	if r.Company == "" {
+		return errParmIsRequired("Company", "string")
+	}
+	if r.Location == "" {
+		return errParmIsRequired("Location", "string")
+	}
+	if r.Link == "" {
+		return errParmIsRequired("Link", "string")
+	}
+	if r.Salary <= 0 {
+		return errParmIsRequired("Salary", "int64")
+	}
+	if r.Remote {
+		return errParmIsRequired("Remote", "bool")
+	}
+	return nil
 }
